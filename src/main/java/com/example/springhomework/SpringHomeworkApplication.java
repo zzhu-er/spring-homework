e84ae6c98b2bdc57e96682f68f19760bf2dce72b
zzhu-er <863955132@qq.com>
2023-02-17 14:22:12 +0800

    Create the skeleton

---
+++ src/main/java/com/example/springhomework/SpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springhomework/SpringHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
