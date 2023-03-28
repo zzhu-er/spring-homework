814cced8c01d3c8a9e939c8254d097b844ef0745
zzhu-er <863955132@qq.com>
2023-03-28 16:10:04 +0800

    chore: create a custom annotation to exclude config and main classes from test report

--- src/main/java/com/example/springhomework/SpringHomeworkApplication.java
+++ src/main/java/com/example/springhomework/SpringHomeworkApplication.java
@@ -1,11 +1,12 @@
 package com.example.springhomework;
 
-import lombok.Generated;
+import com.example.springhomework.utilities.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Generated
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
--- src/main/java/com/example/springhomework/configuration/FeignConfig.java
+++ src/main/java/com/example/springhomework/configuration/FeignConfig.java
@@ -1,10 +1,12 @@
 package com.example.springhomework.configuration;
 
+import com.example.springhomework.utilities.Generated;
 import feign.Logger;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Generated
 @Configuration
 public class FeignConfig {
 
