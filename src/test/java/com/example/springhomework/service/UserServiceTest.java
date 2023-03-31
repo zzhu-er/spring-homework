b78063083630270ad945564d0b0cef099656aa17
zzhu-er <863955132@qq.com>
2023-03-31 12:54:09 +0800

    test: remove all unneeded unit tests

--- src/test/java/com/example/springhomework/service/UserServiceTest.java
+++ src/test/java/com/example/springhomework/service/UserServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.springhomework.service;
 
-import static java.util.Arrays.asList;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -11,9 +10,6 @@ import com.example.springhomework.dto.UserRequest;
 import com.example.springhomework.model.User;
 import com.example.springhomework.repository.UserRepository;
 import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Collections;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -21,10 +17,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -37,170 +29,6 @@ public class UserServiceTest {
   private EmailClient emailClient;
   @InjectMocks
   private UserService subject;
-
-  /*@Test
-  void shouldReturnEmptyListWhenNoData() {
-    //given
-    when(userRepository.findAll()).thenReturn(Collections.emptyList());
-    //when
-    List<User> result = subject.findAll();
-    //then
-    Assertions.assertThat(result).isEqualTo(Collections.emptyList());
-  }*/
-
-  /*@Test
-  void shouldReturnAllUsers() {
-    //given
-    List<User> expect = asList(
-        User.builder().id(0L).age(18L).name("A").createdAt(Instant.now()).updatedAt(Instant.now())
-            .build(),
-        User.builder().id(1L).age(19L).name("B").createdAt(Instant.now()).updatedAt(Instant.now())
-            .build()
-    );
-    when(userRepository.findAll()).thenReturn(expect);
-    //when
-    List<User> result = subject.findAll();
-    //then
-    Assertions.assertThat(result).isEqualTo(expect);
-  }*/
-
-  /*@Test
-  void shouldGetOnlyFirstUserInFirstPageWithPageSizeIs1() {
-    //given
-    List<User> userList = asList(
-        User.builder().id(0L).age(18L).name("A").createdAt(Instant.now()).updatedAt(Instant.now())
-            .build(),
-        User.builder().id(1L).age(19L).name("B").createdAt(Instant.now()).updatedAt(Instant.now())
-            .build()
-    );
-    int page = 0;
-    int size = 1;
-    Pageable pageable = PageRequest.of(page, size);
-    Page<User> expect = new PageImpl<>(userList, pageable, size);
-    when(userRepository.findAll(pageable)).thenReturn(expect);
-    //when
-    List<User> result = subject.findAll(page, size);
-    //then
-    Assertions.assertThat(result).isEqualTo(expect.getContent());
-  }*/
-
-  /*@Test
-  void shouldReturnAllUsersWithAge18WithoutPagination() {
-    //given
-    Long age = 18L;
-    List<User> expect = asList(
-        User.builder().id(0L).age(age).name("A").createdAt(Instant.now()).updatedAt(Instant.now())
-            .build(),
-        User.builder().id(1L).age(age).name("B").createdAt(Instant.now()).updatedAt(Instant.now())
-            .build()
-    );
-    when(userRepository.findByAge(age)).thenReturn(expect);
-    //when
-    List<User> result = subject.findByAge(age);
-    //then
-    Assertions.assertThat(result).isEqualTo(expect);
-  }*/
-
-  /*@Test
-  void shouldGetOnlyFirstUserWithAge18InFirstPageWithPageSizeIs1() {
-    //given
-    Long age = 18L;
-    List<User> userList = asList(
-        User.builder().id(0L).age(age).name("A").createdAt(Instant.now()).updatedAt(Instant.now())
-            .build(),
-        User.builder().id(1L).age(age).name("B").createdAt(Instant.now()).updatedAt(Instant.now())
-            .build()
-    );
-    int page = 0;
-    int size = 1;
-    Pageable pageable = PageRequest.of(page, size);
-    Page<User> expect = new PageImpl<>(userList, pageable, size);
-    when(userRepository.findByAge(pageable, age)).thenReturn(expect);
-    //when
-    List<User> result = subject.findByAge(page, size, age);
-    //then
-    Assertions.assertThat(result).isEqualTo(expect.getContent());
-  }*/
-
-  /*@Test
-  void shouldReturnAllUsersWithNameAWithoutPagination() {
-    //given
-    String name = "A";
-    List<User> expect = asList(
-        User.builder().id(0L).age(18L).name(name).createdAt(Instant.now()).updatedAt(Instant.now())
-            .build(),
-        User.builder().id(1L).age(19L).name(name).createdAt(Instant.now()).updatedAt(Instant.now())
-            .build()
-    );
-    when(userRepository.findByName(name)).thenReturn(expect);
-    //when
-    List<User> result = subject.findByName(name);
-    //then
-    Assertions.assertThat(result).isEqualTo(expect);
-  }*/
-
-  /*@Test
-  void shouldGetOnlyFirstUserWithNameAInFirstPageWithPageSizeIs1() {
-    //given
-    String name = "A";
-    List<User> userList = asList(
-        User.builder().id(0L).age(18L).name(name).createdAt(Instant.now()).updatedAt(Instant.now())
-            .build(),
-        User.builder().id(1L).age(19L).name(name).createdAt(Instant.now()).updatedAt(Instant.now())
-            .build()
-    );
-    int page = 0;
-    int size = 1;
-    Pageable pageable = PageRequest.of(page, size);
-    Page<User> expect = new PageImpl<>(userList, pageable, size);
-    when(userRepository.findByName(pageable, name)).thenReturn(expect);
-    //when
-    List<User> result = subject.findByName(page, size, name);
-    //then
-    Assertions.assertThat(result).isEqualTo(expect.getContent());
-  }*/
-
-  /*@Test
-  void shouldReturnAllUsersBetweenMar7thAndMar9thWithoutPagination() {
-    //given
-    Instant instant = LocalDate.parse("2023-03-08").atStartOfDay(ZoneId.of("UTC")).toInstant();
-    List<User> expect = asList(
-        User.builder().id(0L).age(18L).name("A").createdAt(instant).updatedAt(instant)
-            .build(),
-        User.builder().id(1L).age(19L).name("B").createdAt(instant).updatedAt(instant)
-            .build()
-    );
-    Instant start = LocalDate.parse("2023-03-07").atStartOfDay(ZoneId.of("UTC")).toInstant();
-    Instant end = LocalDate.parse("2023-03-09").atStartOfDay(ZoneId.of("UTC")).toInstant();
-    when(userRepository.findByCreatedAtBetween(start, end)).thenReturn(expect);
-    //when
-    List<User> result = subject.findAllBetweenDates(start, end);
-    //then
-    Assertions.assertThat(result).isEqualTo(expect);
-  }*/
-
-  /*@Test
-  void shouldGetOnlyFirstUserBetweenMar7thAndMar9thInFirstPageWithPageSizeIs1() {
-    //given
-    Instant instant = LocalDate.parse("2023-03-08").atStartOfDay(ZoneId.of("UTC")).toInstant();
-    List<User> userList = asList(
-        User.builder().id(0L).age(18L).name("A").createdAt(instant).updatedAt(instant)
-            .build(),
-        User.builder().id(1L).age(19L).name("B").createdAt(instant).updatedAt(instant)
-            .build()
-    );
-    int page = 0;
-    int size = 1;
-    Pageable pageable = PageRequest.of(page, size);
-    Page<User> expect = new PageImpl<>(userList, pageable, size);
-    Instant start = LocalDate.parse("2023-03-07").atStartOfDay(ZoneId.of("UTC")).toInstant();
-    Instant end = LocalDate.parse("2023-03-09").atStartOfDay(ZoneId.of("UTC")).toInstant();
-    when(userRepository.findByCreatedAtBetween(pageable, start, end)).thenReturn(expect);
-    //when
-    List<User> result = subject.findAllBetweenDates(page, size, start, end);
-    //then
-    Assertions.assertThat(result).isEqualTo(expect.getContent());
-  }*/
 
   @Test
   void shouldSaveSuccessfully() throws Exception {
