d989ab7be204e1e8e5d1e2b7724be16e2838b537
zzhu-er <863955132@qq.com>
2023-04-09 23:54:26 +0800

    feat: enable CORS for user API

--- src/main/java/com/example/springhomework/controller/UserController.java
+++ src/main/java/com/example/springhomework/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
